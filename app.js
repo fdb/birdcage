de565830fd20bd24354b8bdfd6f816a09f8754d2
Frederik De Bleser <frederik@burocrazy.com>
2013-05-28 00:03:23 +0200

    Add (hacky) web server showing latest tweets.

--- app.js
+++ app.js
@@ -38,6 +38,37 @@ birdcage.startFetcher = function(searchTerm) {
   }, 5000);
 };
 
+birdcage.tweetTable = function(tweets) {
+  var html = '<table>';
+  html += '<thead><tr><th>Date</th><th>User</th><th>Text</th></tr></thead>';
+  html += '<tbody>';
+  _.each(tweets, function(tweet) {
+    html += '<tr>';
+    html += '<td>' + tweet.created_at + '</td>';
+    html += '<td>' + tweet.from_user + '</td>';
+    html += '<td>' + tweet.text + '</td>';
+    html += '</tr>';
+  });
+  html += '</tbody>';
+  html += '</table>';
+  return html;
+};
+
+birdcage.startServer = function() {
+  var server = http.createServer(function(req, res) {
+    birdcage.tweets.find().limit(100).sort([['created_at', 'desc']]).toArray(function(err, results) {
+      var html = '<!DOCTYPE html><meta charset="utf-8"><html><head>';
+      html += '<style>body {font:small sans-serif} table {border-collapse: collapse } td {border-top:1px solid #999;padding:10px 5px}</style>';
+      html += '</head><body>';
+      html += birdcage.tweetTable(results);
+      html += '</html>';
+      res.end(html);
+    });
+  });
+  console.log('Listening at http://127.0.0.1:3000/');
+  server.listen(3000, '127.0.0.1');
+};
+
 birdcage.start = function(query) {
   birdcage._db.open(function(err) {
     if (err) {
@@ -49,6 +80,7 @@ birdcage.start = function(query) {
         } else {
           birdcage.tweets = collection;
           birdcage.startFetcher(query);
+          birdcage.startServer();
         }
       });
     }
